230d8039c2ce863d811d011710aac6d1c09a4765
Rv-scoding <roderickventi2@gmail.com>
2026-09-08 14:32:55 -0400

    A

---
+++ app/src/main/java/edu/temple/viewworking/MainActivity.kt
@@ -0,0 +1,29 @@
+package edu.temple.viewworking
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val imageView : ImageView = findViewById(R.id.imageView)
+        var id: Int = android.R.drawable.star_big_on
+
+        findViewById<Button>(R.id.button).setOnClickListener{
+            if (id == R.drawable.dark_galaxy) {
+                imageView.setImageResource(R.drawable.normal_galaxy)
+                id = R.drawable.normal_galaxy
+            }
+            else {
+                imageView.setImageResource(R.drawable.dark_galaxy)
+                id = R.drawable.dark_galaxy
+
+            }
+        }
+    }
+}
